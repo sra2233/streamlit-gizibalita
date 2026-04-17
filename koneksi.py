7e2a7d567907a933ff65a9164cf83bbe665c3c10
sra2233 <156988049+sra2233@users.noreply.github.com>
2026-04-17 17:36:52 +0700

    Update koneksi.py

--- koneksi.py
+++ koneksi.py
@@ -1,12 +1,12 @@
-import mysql.connector
 import streamlit as st
+import mysql.connector
 
 def koneksi_db():
     conn = mysql.connector.connect(
-        host=st.secrets["mysql.railway.internal"],
-        user=st.secrets["root"],
-        password=st.secrets["KIUEBrtOGqRgYIYstrlIWnKLvCacStYh"],
-        database=st.secrets["railway"],
+        host=st.secrets["MYSQLHOST"],
+        user=st.secrets["MYSQLUSER"],
+        password=st.secrets["MYSQLPASSWORD"],
+        database=st.secrets["MYSQLDATABASE"],
         port=int(st.secrets["MYSQLPORT"])
     )
     return conn
