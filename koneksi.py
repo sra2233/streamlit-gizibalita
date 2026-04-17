276964e687acc575f9b256d54d8ca351c3e6f3c0
sra2233 <156988049+sra2233@users.noreply.github.com>
2026-04-17 17:16:42 +0700

    Update koneksi.py

--- koneksi.py
+++ koneksi.py
@@ -1,12 +1,10 @@
 import mysql.connector
 
 def koneksi_db():
-
     conn = mysql.connector.connect(
         host="localhost",
         user="root",
         password="",
         database="posyandu"
     )
-
     return conn
